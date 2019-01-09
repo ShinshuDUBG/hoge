0965005644384e9bedd8fb109dc30f85744bae06
fujitetsu1031 <16t2144c@shinshu-u.ac.jp>
2019-01-09 17:11:01 +0900

    first commit

---
+++ DUBG.main/Assets/bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    Rigidbody rd;
+    // Use this for initialization
+    void Start()
+    {
+        rd = GetComponent<Rigidbody>();
+        rd.AddForce(transform.forward * 1000f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.transform.tag == "Ground")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
