e3809fa759c37a6ed6bd086e52e5b26ff95b7675
siren666 <16t2161c@shinshu-u.ac.jp>
2019-01-16 13:55:45 +0900

    Add1 UI

---
+++ DUBG.main/Assets/timer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour {
+    float time;
+    Text text;
+
+    void Start()
+    {
+        time = 0;
+        text = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        int minute = (int)time / 60;
+        int second = (int)time % 60;
+        string minText, secText;
+        if (minute < 10)
+            minText = "0" + minute.ToString();
+        else
+            minText = minute.ToString();
+        if (second < 10)
+            secText = "0" + second.ToString();
+        else
+            secText = second.ToString();
+
+        text.text = minText + ":" + secText;
+    }
+}
---
+++ DUBG.main/Assets/zandan.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class zandan : MonoBehaviour {
+    bool isRunning = false;
+    public int shoutcount = 100;
+    public GameObject bullet;
+    Rigidbody rd;
+    float TimeCount = 2; 
+
+
+
+
+    // Use this for initialization
+    void Start () {
+        this.GetComponent<Text>().text = "×" + shoutcount;
+      
+
+
+
+    }
+
+        // Update is called once per frame
+        void Update()
+    {
+        
+
+        if (Input.GetButton("reload") )
+        {
+            TimeCount -= Time.deltaTime;
+            if (TimeCount <= 0)
+            {
+           
+            shoutcount = 100;
+            this.GetComponent<Text>().text = "×" + shoutcount;
+                TimeCount = 2;
+            }
+
+
+          
+
+        }
+
+        if (Input.GetAxisRaw("Fire") == 1)
+        {
+            if (shoutcount < 1)
+
+                return;
+            shoutcount -= 1;
+            this.GetComponent<Text>().text = "×" + shoutcount;
+            
+
+
+        }
+            
+
+
+
+    }
+    
+
+}
